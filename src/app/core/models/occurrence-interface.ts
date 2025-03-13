61f1c6115343888ca8141cf0fac45d1fe57bfe06
Luiz Gustavo da Silva <38591825+Luiz4us8av6@users.noreply.github.com>
2025-03-13 18:22:09 -0300

    update: apagando código obsoleto

--- src/app/core/models/occurrence-interface.ts
+++ src/app/core/models/occurrence-interface.ts
@@ -1,3 +1,6 @@
+import { Report } from "./report-interface";
+import { User } from "./user-interface";
+
 export interface DataOccurrence {
   count: number;
   occurrences: Occurrence[];
@@ -12,35 +15,6 @@ export interface Occurrence {
   title?: string | null;
   report: Report;
   user: User;
-}
-
-export interface Report {
-  id: number;
-  userId: number | null;
-  code: string;
-  description: string;
-  status: string;
-  createdAt: string;
-  updatedAt: string;
-  reporterName: string;
-  CPF?: string | null;
-  telephone?: string | null;
-  address?: string | null;
-  email?: string | null;
-  addressReport?: string | null;
-  cityReport?: string | null;
-  UFReport?: string | null;
-  countryReport?: string | null;
-  titleReport?: string | null;
-}
-
-export interface User {
-  id: number;
-  name: string;
-  email: string;
-  password: string;
-  createdAt: string;
-  updatedAt: string;
 }
 
 export interface OccurrenceReq {
